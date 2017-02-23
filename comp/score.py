613cf94071cd7bae072d125232a342f64c7bb712
istorian <liv.basilis@protonmail.ch>
2017-02-23 21:08:20 +0200

    NOn-negative scoring

--- comp/score.py
+++ comp/score.py
@@ -1,7 +1,7 @@
 def score(lats, clats, reqs, sol):
     s = 0
     for (v, e, r) in reqs:
-        m = 1001
+        m = lats[e]
         for c, l in clats[e].iteritems():
             if((v in sol[c]) and (l < m)):
                 m = l
