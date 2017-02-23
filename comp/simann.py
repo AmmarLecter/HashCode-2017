576d5944fc592fb1d368d5af7ca086bb154a4b0e
Nick Vathis <nvathis@softlab.ntua.gr>
2017-02-23 21:43:13 +0200

    i am idiot

--- comp/simann.py
+++ comp/simann.py
@@ -6,7 +6,7 @@ from score import score
 def simann(firstsol, V, E, R, C, X, sizes, latencies, cache_latencies, requests):
     Y = 10
     N = 1000
-    best_score = 0
+    best_score = score(firstsol)
     best_sol = [set() for x in range(C)]
 
     def cache_size(cacheid):
