91741561eaa45fb8e680fce8b57195a1da8b1fa2
Nick Vathis <nvathis@softlab.ntua.gr>
2017-02-23 21:03:29 +0200

    poios malakas ekane git add everything?

--- comp/score.py
+++ comp/score.py
@@ -1,8 +1,8 @@
 def score(lats, clats, reqs, sol):
     s = 0
     for (v, e, r) in reqs:
-        m = 1001 
-        for (c,l) in clats[e]:
+        m = 1001
+        for c, l in clats[e].iteritems():
             if((v in sol[c]) and (l < m)):
                 m = l
         s += r*(lats[e] - m)
