0cf586c84675315c556afe076d015e271a8f85f3
Nick Vathis <nvathis@softlab.ntua.gr>
2017-02-23 22:03:36 +0200

    foo.rb

--- comp/foo.rb
+++ comp/foo.rb
@@ -1,13 +1,24 @@
 require 'parallel'
 require 'open3'
 
-results = Parallel.map(1..100, in_threads: 4) {
-	i, o, e = Open3::popen3("python #{ARGV[0]} < #{ARGV[1]}")
+times = 1..5
+#testcases = ['kittens.in', 'me_at_the_zoo.in', 'statement.in', 'trending_today.in', 'videos_worth_spreading.in']
+testcases = ['statement.in']
+
+executions = times.zip(testcases.cycle)
+
+results = Parallel.map(executions, in_threads: `cat /proc/cpuinfo | grep 'core id'`.to_i) { |testcase|
+	testcase = testcase[1]
+	i, o, e = Open3::popen3("python simannmain.py < #{testcase}")
 	
-	[o.read, e.read]
+	[testcase, o.read, e.read]
 }
 
-res = results.max_by { |x| x[1] }
+p results
 
-puts res[0]
-STDERR.puts res[1]
+res = results.group_by { |x| x[0] }.map { |key, val| val.max_by { |x| x[2] } }
+
+res.each do |r|
+	File.write("outs/#{r[0]}.out", r[1])
+	File.write("outs/#{r[0]}.err", r[2])
+end
